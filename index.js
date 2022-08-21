4b55d44fe04f66e9ce6f0c3e9c1df7133d01522b
Mathews <mathews.milimu@student.moringaschool.com>
2022-08-21 15:35:06 +0300

    Lab done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+var  customerName = "bob"
+var bestCustomer;
+const leastcustomer = "Colo"
+
+function upperCaseCustomerName (){
+    
+    customerName = customerName.toUpperCase();
+    return customerName
+}
+
+
+
+function setBestCustomer (){
+    bestCustomer = "not bob"
+    
+    return bestCustomer
+
+}
+
+function overwriteBestCustomer (){
+    bestCustomer = "maybe bob"
+    return bestCustomer
+}
+
+function changeLeastFavoriteCustomer (){
+    
+     leastcustomer = "Junior"
+
+}
+
 // Write your solution in this file!
